e0b21002627708a40baff6bd35cb5ad98bc29472
cg <qian_qye@163.com>
2018-10-13 09:24:31 +0800

    firt commit

---
+++ consurrency/src/main/java/cn/lvycg/consurrency/ConsurrencyApplication.java
@@ -0,0 +1,12 @@
+package cn.lvycg.consurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsurrencyApplication.class, args);
+	}
+}
